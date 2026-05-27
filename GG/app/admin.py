d2428c493f599f148a00dca845903eebb6866f12
Mito2399 <144456238+Mito2399@users.noreply.github.com>
2026-05-27 11:24:32 +0800

    Delete GG/app directory

--- GG/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ClientPersonalInfo, ClientStatus, UserLog, Beneficiary, Booking
-
-admin.site.register(ClientPersonalInfo)
-admin.site.register(ClientStatus)
-admin.site.register(UserLog)
-admin.site.register(Beneficiary)
-admin.site.register(Booking)
