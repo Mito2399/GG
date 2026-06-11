5b75c3f1ca3892f6e9bd825063a5cfc5c3e4a207
Mito2399 <144456238+Mito2399@users.noreply.github.com>
2026-06-11 10:59:24 +0800

    Delete GG/app directory

--- GG/app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import ClientPersonalInfo, ClientStatus, UserLog, Beneficiary, Booking, Payment
-
-admin.site.register(ClientPersonalInfo)
-admin.site.register(ClientStatus)
-admin.site.register(UserLog)
-admin.site.register(Beneficiary)
-admin.site.register(Booking)
-admin.site.register(Payment)   # FIX: Payment was missing from admin registration
