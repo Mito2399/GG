fc450b3fb12c76d7a4d91f490d6ad13d00efaec3
Nomer John Casinio <144456238+Mito2399@users.noreply.github.com>
2026-09-16 14:08:04 +0800

    Update urls.py

--- GG/GG/urls.py
+++ GG/GG/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("aprilPogi/", admin.site.urls),
     path("",       include("app.urls")),
 ]
 
